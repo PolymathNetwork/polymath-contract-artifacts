bda4830e959b24b0ead511294cb51c2f73fb8941
shuffledex <shuffledex@gmail.com>
2019-01-02 15:54:45 -0300

    Contract artifacts

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# polymath-contract-artifacts
+## @polymath-core-artifacts
+
+Smart contract compilation artifacts for Polymath v2.1.0.
+
+## Installation
+
+```bash
+yarn add polymath-core-artifacts
+```
+
+**Import**
+
+```typescript
+import * as artifacts from 'polymath-core-artifacts';
+```
+
+or
+
+```javascript
+var artifacts = require('polymath-core-artifacts');
+```
+
+## Contributing
+
+We welcome improvements and fixes from the wider community! To report bugs within this package, please create an issue in this repository.
+
+### Install dependencies
+
+If you don't have yarn workspaces enabled (Yarn < v1.0) - enable them:
+
+```bash
+yarn config set workspaces-experimental true
+```
+
+Then install dependencies
+
+```bash
+yarn install
+```
+
+### Build
+
+To build this package run the following from root directory:
+
+```bash
+yarn build
+```
+
+### Clean
+
+```bash
+yarn clean
+```
+
+### Lint
+
+```bash
+yarn lint
+```
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import * as CappedSTO from '../artifacts/CappedSTO.json';
+import * as CappedSTOFactory from '../artifacts/CappedSTOFactory.json';
+import * as ERC20DividendCheckpoint from '../artifacts/ERC20DividendCheckpoint.json';
+import * as EtherDividendCheckpoint from '../artifacts/EtherDividendCheckpoint.json';
+import * as FeatureRegistry from '../artifacts/FeatureRegistry.json';
+import * as GeneralPermissionManager from '../artifacts/GeneralPermissionManager.json';
+import * as GeneralTransferManager from '../artifacts/GeneralTransferManager.json';
+import * as IModule from '../artifacts/IModule.json';
+import * as ISTO from '../artifacts/ISTO.json';
+import * as ITransferManager from '../artifacts/ITransferManager.json';
+import * as ManualApprovalTransferManager from '../artifacts/ManualApprovalTransferManager.json';
+import * as ModuleFactory from '../artifacts/ModuleFactory.json';
+import * as ModuleRegistry from '../artifacts/ModuleRegistry.json';
+import * as Ownable from '../artifacts/Ownable.json';
+import * as PolymathRegistry from '../artifacts/PolymathRegistry.json';
+import * as PolyToken from '../artifacts/PolyToken.json';
+import * as PolyTokenFaucet from '../artifacts/PolyTokenFaucet.json';
+import * as SecurityToken from '../artifacts/SecurityToken.json';
+import * as SecurityTokenRegistry from '../artifacts/SecurityTokenRegistry.json';
+import * as USDTieredSTO from '../artifacts/USDTieredSTO.json';
+import * as USDTieredSTOFactory from '../artifacts/USDTieredSTOFactory.json';
+
+export {
+  CappedSTO,
+  CappedSTOFactory,
+  ERC20DividendCheckpoint,
+  EtherDividendCheckpoint,
+  FeatureRegistry,
+  GeneralPermissionManager,
+  GeneralTransferManager,
+  IModule,
+  ISTO,
+  ITransferManager,
+  ManualApprovalTransferManager,
+  ModuleFactory,
+  ModuleRegistry,
+  Ownable,
+  PolymathRegistry,
+  PolyToken,
+  PolyTokenFaucet,
+  SecurityToken,
+  SecurityTokenRegistry,
+  USDTieredSTO,
+  USDTieredSTOFactory
+};
