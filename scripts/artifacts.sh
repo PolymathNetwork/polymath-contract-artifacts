614d1420f10afe105d1a204ad5619925312d8739
shuffledex <shuffledex@gmail.com>
2019-01-08 14:17:25 -0300

    update bash script

--- scripts/artifacts.sh
+++ scripts/artifacts.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# to run: sh artifacts.sh dev-2.1.0 v2.1.0-beta "Auto release 2.1.0"
+# to run: sh artifacts.sh dev-2.1.0 v2.1.0-beta "Auto release 2.1.0" true
 
 # define absolute path to polymath-core and polymath-contract-artifacts projects
 PATH_CORE="/Users/polymath/polymath-core"
 PATH_CONTRACT_ARTIFACTS="/Users/polymath/polymath-contract-artifacts"
+
+# define absolute path to polymath-abi-wrappers if you want automate the json to ts transformation
+PATH_ABI_WRAPPERS="/Users/polymath/polymath-abi-wrappers"
 
 # params validation
 if [[ -z "$1" ]]; then
@@ -35,4 +38,13 @@ done
 # build the new json abi files, push it and tag it to a new version
 cd $PATH_CONTRACT_ARTIFACTS
 yarn clean && yarn build:ci
-git add --all && git commit -m "$3" && git tag -a $2 -m "$3" && git push origin $2
+git add --all && git commit -m "$3" && git push origin master && git tag -a $2 -m "$3" && git push origin $2
+
+echo "\nArtifacts successfully updated..."
+
+# start abi to ts generator process if user want it
+if [[ -n "$4" ]]; then
+  echo "\nStarting polymath-abi-wrappers process..."
+  cd $PATH_ABI_WRAPPERS
+  sh scripts/generator.sh $2
+fi
